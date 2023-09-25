93c9733ec7013fc38911da98d114a47dc794dbb7
John Sellers <sellers.e.john@gmail.com>
2023-09-24 20:58:30 -0400

    , added

--- authentication_lambda.py
+++ authentication_lambda.py
@@ -40,7 +40,7 @@ def lambda_handler(event, context):
         print(
             "FaceId: ",
             match["Face"]["FaceId"],
-            "Confidence %: ",
+            ", " "Confidence %: ",
             match["Face"]["Confidence"],
         )
 
